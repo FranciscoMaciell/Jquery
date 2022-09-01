037b015898baa305c39731fc2e0096e65620ad30
Francisco Maciel <109698009+FranciscoMaciell@users.noreply.github.com>
2022-09-01 12:50:28 -0300

    att jQuery

--- projeto-lista-tarefas/js/controle.js
+++ projeto-lista-tarefas/js/controle.js
@@ -1,59 +1,49 @@
-let cnt=0; 
+let input=$('#inputTarefa');
+let btAdd=$('#btn-add');
+let main=$('#areaLista');
+let del=$('.delete');
+let cnt=0;
 
-$(document).ready(function () {
 
-    let botao=$('#btn-add');
-    let inputText=$('#inputTarefa');
-    let Deletar=$('.item-botao');
 
-    botao.click(function () {
+input.keyup(function(event){
+    if(event.keyCode===13){
+        event.preventDefault();
         addTarefa();
-    })
-
-    inputText.keyup(function(event){
-        if(event.keyCode===13){
-            event.preventDefault();
-            addTarefa()
-        }
-    })
-
-
+    }
 })
 
 
 
 
-/* --- DESENVOLVIMENTO DE FUNÇÕES --- */
-function addTarefa() {
-    let valorInput = $('#inputTarefa').val();
-    if (valorInput !== "" && valorInput !== null && valorInput !== undefined) {
+
+
+/* --- DESENVOLVIMENTO DAS FUNÇÕES --- */
+function addTarefa(){
+    let inputText=input.val();
+    if(inputText!==""&&inputText!==null&&inputText!==undefined){
         ++cnt;
-        let novoItem =
-            `<div id="${cnt}"class="item">
+        let novoItem=
+        `<div id="${cnt}" class="item">
             <div class="item-icone">
                 <i class="mdi mdi-circle-outline"></i>
             </div>
             <div class="item-nome">
-                ${valorInput}
+                ${inputText}
             </div>
             <div class="item-botao">
-                <button class="delete"><i class="mdi mdi-delete"></i>Deletar</button>
+                <button onclick="deletar(${cnt})" class="delete"><i class="mdi mdi-delete"></i> Deletar</button>
             </div>
         </div>`
 
-        $('#areaLista').append(novoItem);
-
-        $('#inputTarefa').val('');
-        $('#inputTarefa').focus();
-
-        $('.delete').click(function(){
-            apaga($('.item').attr('id'))
-        })
+        main.append(novoItem);
+        input.val('');
+        input.focus();
     }
 }
 
-function apaga(id){
-    let apagar=id;
-    apagar.remove();
+function deletar(id){
+    let tarefa=document.getElementById(id);
+    tarefa.remove();
 }
 
